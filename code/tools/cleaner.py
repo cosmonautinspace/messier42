6ab65c500d1f820ca7f68e948b1e0179dacc1c17
Haani Ansari <haani.ansari@uni-potsdam.de>
2025-01-09 12:10:06 +0100

    fixed some stuff

--- code/tools/cleaner.py
+++ code/tools/cleaner.py
@@ -1,10 +1,10 @@
 import pandas as pd 
 
-data = pd.read_csv('code/tools/input2.csv')
+data = pd.read_csv('data/scrapeFromHere/pixelValues.csv')
 
 
 
-#Since CMOS sensors use Bayer pattern which has a 1:2:1 ratio of RGB subpixels, 
+#Since CMOS sensors employ the Bayer pattern, which has a 1:2:1 ratio of RGB subpixels, 
 #multiplication factor for Green needs to be calculated seperately.
 #Also, this theoretically helps with green noise removal.
 
@@ -13,6 +13,7 @@ rbMfactor = []
 #green subpixel multiplication factor
 gMfactor = []
 
+#calculating the multiplication factors for
 for index, row in data.iterrows():
     rFactor = int(row['rT']/row['rI'])
     bFactor = int(row['bT']/row['bI'])
@@ -23,7 +24,31 @@ for index, row in data.iterrows():
 
 data.insert(loc=0, column='rbM', value=rbMfactor)
 data.insert(loc=0, column='gM', value=gMfactor)
-data = data.drop(columns=['rT','gT','bT'])
+
+#The raw target pixel values are dropped as they are not needed once the multiplication factor has been calculated
+data = data.drop(columns=['rT','gT','bT'])q
 data = data.drop_duplicates()
+'''
+#normalization
+#The maximum possible theoretical multiplaction factor for a pixel is 255 (when value is amplified from 1 to 255)
+#Also the maximum possible value for a pixel is 255, so both the raw pixel values and the MFactor as divided by 255 for normalization
+'''
+
+'''
+for i in range(data['gM'].size):
+    data['gM'].iloc[[i]] /= 255
+    data['rbM'].iloc[[i]] /= 255
+    data['gI'].iloc[[i]] /= 255
+    data['rI'].iloc[[i]] /= 255
+    data['bI'].iloc[[i]] /= 255
+'''
+data.reset_index(drop=True, inplace=True)
+
+###############Outlier removal###############
+#quartile filter
+
+#zscore filter
+
 print(data)
+
 data.to_csv('test.csv', sep=',', index=False)
