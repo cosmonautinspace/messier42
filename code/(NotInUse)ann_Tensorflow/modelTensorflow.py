e231022e16613307c8639865094548fc15709747
Haani Ansari <haani.ansari@uni-potsdam.de>
2025-01-14 13:05:56 +0100

    Added basic code for the ann model(pybrain) under /code/ann_pybrain/model.py

---
+++ code/(NotInUse)ann_Tensorflow/modelTensorflow.py
@@ -0,0 +1,1 @@
+'''If tensorflow works better add code for the model here'''
---
+++ code/ann_PyBrain/model.py
@@ -0,0 +1,116 @@
+import pandas as pd 
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import SupervisedDataSet
+from pybrain.structure import FeedForwardNetwork
+from pybrain.supervised import BackpropTrainer
+from pybrain.structure.modules import SoftmaxLayer, LinearLayer, SigmoidLayer, TanhLayer, ReluLayer
+#from pybrain.utilities import percentError
+from pybrain.structure import FullConnection
+from pybrain.tools.customxml.networkwriter import NetworkWriter
+from pybrain.tools.customxml.networkreader import NetworkReader
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+import statsmodels.api as sm
+
+
+'''Dataset configuration:
+RGB subpixel values as input variables (3)
+RedBlueMultiplicationFactor and GreenMultiplicationFactor as the target variables (2)'''
+
+data = pd.read_csv('data/extras/cleanedDataWhole.csv')
+
+alldataG = SupervisedDataSet(inp=1,target=1)
+alldataRB = SupervisedDataSet(inp=2,target=1)
+for i in range(data['rI'].size):
+    alldataG.addSample(inp=data.gI[i], target=data.gM[i])
+
+for i in range(data['rI'].size):
+    alldataRB.addSample(inp=[data.rI[i],data.bI[i]], target=[data.rbM[i]])
+
+
+testG, trainG = alldataG.splitWithProportion(0.20)
+
+testRB, trainRB = alldataRB.splitWithProportion(0.20)
+
+
+'''Model configuration can be found below, need to optimize before final submission of the project....Just placeholder values for now, for testing 
+purposes'''
+
+hiddenLayers = 100
+rounds = 5
+
+'''
+ann = FeedForwardNetwork()
+
+inLayer = LinearLayer(inputLayers)
+hiddenLayer = SoftmaxLayer(hiddenLayers)
+outLayer = LinearLayer(outputLayers)
+ann.addInputModule(inLayer)
+ann.addModule(hiddenLayer)
+ann.addOutputModule(outLayer)
+
+inToHidden = FullConnection(inLayer,hiddenLayer)
+hiddenToOut = FullConnection(hiddenLayer,outLayer)
+ann.addConnection(inToHidden)
+ann.addConnection(hiddenToOut)
+ann.sortModules()
+'''
+
+annRB = FeedForwardNetwork()
+
+inLayer = LinearLayer(2)
+hiddenLayer = TanhLayer(hiddenLayers)
+outLayer = LinearLayer(1)
+annRB.addInputModule(inLayer)
+annRB.addModule(hiddenLayer)
+annRB.addOutputModule(outLayer)
+
+inToHidden = FullConnection(inLayer,hiddenLayer)
+hiddenToOut = FullConnection(hiddenLayer,outLayer)
+annRB.addConnection(inToHidden)
+annRB.addConnection(hiddenToOut)
+annRB.sortModules()
+
+annG = FeedForwardNetwork()
+
+inLayer = LinearLayer(1)
+hiddenLayer = TanhLayer(hiddenLayers)
+outLayer = LinearLayer(1)
+annG.addInputModule(inLayer)
+annG.addModule(hiddenLayer)
+annG.addOutputModule(outLayer)
+
+inToHidden = FullConnection(inLayer,hiddenLayer)
+hiddenToOut = FullConnection(hiddenLayer,outLayer)
+annG.addConnection(inToHidden)
+annG.addConnection(hiddenToOut)
+annG.sortModules()
+
+
+'''Training'''
+'''
+trainer = BackpropTrainer(ann, dataset=train, verbose=True, learningrate=0.00001)
+for i in range(rounds):
+    trainer.trainEpochs(1)
+'''
+
+trainer = BackpropTrainer(annRB, dataset=trainRB, verbose=True, learningrate=0.00001)
+for i in range(rounds):
+    trainer.trainEpochs(1)
+
+trainer = BackpropTrainer(annG, dataset=trainG, verbose=True, learningrate=0.00001)
+for i in range(rounds):
+    trainer.trainEpochs(1)
+
+NetworkWriter.writeToFile(annRB, "modelRedBlue.xml")
+NetworkWriter.writeToFile(annG, "modelGreen.xml")
+
+print(testRB)
+print(annRB.activateOnDataset(testRB))
+
+print(testG)
+print(annG.activateOnDataset(testG))
+
+print(annRB.activate([50,50]))
