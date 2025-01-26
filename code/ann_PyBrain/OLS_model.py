59c53212fb0eaa3ec8c32b1cd6ef6e887cabcd3f
nooo0b <110136042+nooo0b@users.noreply.github.com>
2025-01-26 10:45:19 +0100

    Add OLS model with model summary and results saving

---
+++ code/ann_PyBrain/OLS_model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+import pickle
+
+data = pd.read_csv('data/cleanedData/cleanedDataWhole.csv')
+X_rb = data[['rI', 'bI']]
+y_rb = data['rbM']
+
+X_g = data[['gI']]
+y_g = data['gM']
+
+X_rb_train, X_rb_test, y_rb_train, y_rb_test = train_test_split(X_rb, y_rb, test_size=0.2, random_state=44)
+X_g_train, X_g_test, y_g_train, y_g_test = train_test_split(X_g, y_g, test_size=0.2, random_state=44)
+
+X_rb_train_sm = sm.add_constant(X_rb_train)
+ols_rb_model = sm.OLS(y_rb_train, X_rb_train_sm).fit()
+X_rb_test_sm = sm.add_constant(X_rb_test)
+y_rb_pred = ols_rb_model.predict(X_rb_test_sm)
+
+X_g_train_sm = sm.add_constant(X_g_train)
+ols_g_model = sm.OLS(y_g_train, X_g_train_sm).fit()
+X_g_test_sm = sm.add_constant(X_g_test)
+y_g_pred = ols_g_model.predict(X_g_test_sm)
+
+with open("/learningBase/currentOlsSolution_RB.pkl", "wb") as f:
+    pickle.dump(ols_rb_model, f)
+with open("/learningBase/currentOlsSolution_G.pkl", "wb") as f:
+    pickle.dump(ols_g_model, f)
+
+with open("/learningBase/currentOlsSolution_RB.txt", "w") as f:
+    f.write(ols_rb_model.summary().as_text())
+with open("/learningBase/currentOlsSolution_G.txt", "w") as f:
+    f.write(ols_g_model.summary().as_text())
+
+print("OLS models saved.")
