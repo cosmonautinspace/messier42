4dae436113bf83992233c215ba424e67145fb96c
nooo0b <110136042+nooo0b@users.noreply.github.com>
2025-01-28 11:06:08 +0100

    Add visualization for training and testing curves, diagnostic plots, scatter plots

--- code/tools/visualiser.py
+++ code/tools/visualiser.py
@@ -1,14 +1,40 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-'''Some test visualisations nothing important as of yet'''
+rb_train_losses = np.loadtxt("code/ann_PyBrain/rb_train_losses.csv", delimiter=",")
+rb_test_losses = np.loadtxt("code/ann_PyBrain/rb_test_losses.csv", delimiter=",")
+g_train_losses = np.loadtxt("code/ann_PyBrain/g_train_losses.csv", delimiter=",")
+g_test_losses = np.loadtxt("code/ann_PyBrain/g_test_losses.csv", delimiter=",")
 
-df = pd.read_csv('data/extras/orion.tif_16bitvalues_preproc_cleanedDataWhole.csv')
+rb_residuals = np.loadtxt("code/ols/rb_ols_residuals.csv", delimiter=",")
+g_residuals = np.loadtxt("code/ols/g_ols_residuals.csv", delimiter=",")
 
-#plt.scatter(df['gM'], df['gI'], c='green')
-plt.scatter(df['rbM'], df['rI'], c='red')
-#plt.scatter(df['rbM'], df['bI'], c='blue')
+plt.figure(figsize=(10, 6))
+plt.plot(rb_train_losses, label="RB Train Loss")
+plt.plot(rb_test_losses, label="RB Test Loss")
+plt.plot(g_train_losses, label="G Train Loss")
+plt.plot(g_test_losses, label="G Test Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.title("Training and Testing Loss Curves")
+plt.savefig("training_testing_curves.png")
 
+plt.figure(figsize=(10, 6))
+plt.hist(rb_residuals, bins=30, alpha=0.5, label="RB Residuals")
+plt.hist(g_residuals, bins=30, alpha=0.5, label="G Residuals")
+plt.xlabel("Residuals")
+plt.ylabel("Frequency")
+plt.legend()
+plt.title("Residual Diagnostic Plots")
+plt.savefig("residual_diagnostic_plots.png")
 
-plt.show()
+plt.figure(figsize=(10, 6))
+plt.scatter(range(len(rb_residuals)), rb_residuals, alpha=0.5, label="RB Residuals")
+plt.scatter(range(len(g_residuals)), g_residuals, alpha=0.5, label="G Residuals")
+plt.xlabel("Index")
+plt.ylabel("Residuals")
+plt.legend()
+plt.title("Residual Scatter Plots")
+plt.savefig("residual_scatter_plots.png")
