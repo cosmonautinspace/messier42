bef4a56da9564bb02b9c23512d5f83b65fe0b2f2
Haani Ansari <haani.ansari@uni-potsdam.de>
2025-02-01 23:04:38 +0100

    edited the editor.py file in the codebase images folder to align with the task description

--- images/codeBase_messier42/editor.py
+++ images/codeBase_messier42/editor.py
@@ -4,17 +4,18 @@ from PIL import Image
 from pybrain.tools.customxml.networkreader import NetworkReader
 import math
 import pandas as pd
+import statsmodels.api as sm
 '''This script is for the final adjustment of pixel values of a given unstrectched image'''
 
 #greenModel = input("Enter the name of the model to use for the Green Channel >> ")
 #redModel = input("Enter the name of the model to use for the Red and Blue channels >> ")
 #inputImage=input("Enter the name of the input image(placed in the input folder) >> ")
 
-
-
 image = Image.open(f'/tmp/ai_system/activation_image.tif')
 annG = NetworkReader.readFrom(f"/tmp/ai_system/currentSolution.xml")
 activationdf = pd.read_csv(f'/tmp/ai_system/activation_data.csv')
+olsG = sm.load('/tmp/ai_system/currentOlsSolution_G.pkl')
+olsRB = sm.oad('/tmp/ai_system/currentOlsSolution_RB')
 
 '''renormalization of values'''
 normDf = pd.read_csv('/tmp/ai_system/normFactors.csv')
@@ -31,7 +32,15 @@ rSD = normDf.rISD[0]
 gSD = normDf.gISD[0]
 bSD = normDf.bISD[0]
 
+print(f"The ANN activation for the given input csv file is (mean normalized) >> 
+      {annG.activate([activationdf.rI[0], activationdf.gI[0],activationdf.bI[0]])}")
 
+x_rb = sm.add_constant(activationdf[['rI', 'bI']])
+x_g = sm.add_constant(activationdf['gI'])
+print(f"The ANN activation for the given input csv file is (mean normalized) >> 
+      {olsRB.predict(x_rb)} {olsG.predict(x_g)}")
+
+print("The ANN will now be activated for the entire image. This might take a while, please wait....")
 for r in range(image.height):
     for c in range(image.width):
         pixelValue = image.getpixel((c,r))
@@ -40,6 +49,5 @@ for r in range(image.height):
         green = (pixelValue[1]*((rbM[0]*gMSD)+gMMean))
         blue = (pixelValue[2]*((rbM[1]*rbMSD)+rbMMean))
         image.putpixel((c,r), (round(red), round(green), round(blue)))
-
-
-image.save(f'/tmp/ai_system/outputANN.tif')
+print("Image saved to the shared folder")
+image.save(f'/tmp/ai_system/output.tif')
